6d5e856e15e57b99a0c7111e9a9aca7438612fde
Bartosz Milczarek <bartosz@milczarek.it>
2021-04-06 14:27:54 +0200

    BitbayBalancesHistoryQuery add field sort

--- xchange-bitbay/src/main/java/org/knowm/xchange/bitbay/v3/dto/trade/BitbayBalancesHistoryQuery.java
+++ xchange-bitbay/src/main/java/org/knowm/xchange/bitbay/v3/dto/trade/BitbayBalancesHistoryQuery.java
@@ -1,6 +1,9 @@
 package org.knowm.xchange.bitbay.v3.dto.trade;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import java.util.List;
+import java.util.Map;
 import lombok.Data;
 
 @Data
@@ -11,4 +14,6 @@ public class BitbayBalancesHistoryQuery {
   private List<String> types;
   private Long fromTime;
   private Long toTime;
+  private List<Map<String, String>> sort =
+      ImmutableList.of(ImmutableMap.of("by", "time", "order", "ASC"));
 }
